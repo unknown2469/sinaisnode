df44eb113365a2a2b6e91e7673183181ac5e1368
unknown2469 <152039395+unknown2469@users.noreply.github.com>
2023-11-26 19:41:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ function scheduleOpportunities(bot, times) {
     const rule = new schedule.RecurrenceRule();
     rule.hour = parseInt(hour, 10);
     rule.minute = parseInt(minute, 10);
+    rule.tz = 'America/Sao_Paulo'; // Substitua pelo fuso horário desejado
 
     schedule.scheduleJob(rule, () => enviarOportunidade(bot, GRUPO_ID));
   });
@@ -94,7 +95,7 @@ function main() {
 
   const horarios = ["07:04", "09:50", "10:55", "11:37", "11:45", "12:49", "13:47", "14:02",
     "16:10", "16:17", "16:41", "17:31", "17:45", "18:18", "18:30",
-    "18:55", "19:37", "21:30", "22:37", "23:59", "02:33", "04:23", "06:46"];
+    "18:55", "19:45", "21:30", "22:37", "23:59", "02:33", "04:23", "06:46"];
 
   scheduleOpportunities(bot, horarios);
 }
