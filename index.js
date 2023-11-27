a783cdc57cbd8f9cef1b26e65ae40c20d9b02166
unknown2469 <152039395+unknown2469@users.noreply.github.com>
2023-11-26 21:15:43 -0300

    Update index novooo.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function main() {
     res.sendStatus(200);
   });
 
-  bot.setWebHook(`https://seu-app-heroku.herokuapp.com/${"6779734021:AAE_qVNt11LN0Z0qPrc0SAD_I3ZEiBCKRnk"}`);
+  bot.setWebHook(`https://sinaisnode-a9178c5335a9.herokuapp.com/${TOKEN}`);
 
   app.listen(PORT, () => {
     console.log(`Servidor está ouvindo na porta ${PORT}`);
@@ -95,7 +95,7 @@ function main() {
 
   const horarios = ["07:04", "09:50", "10:55", "11:37", "11:45", "12:49", "13:47", "14:02",
     "16:10", "16:17", "16:41", "17:31", "17:45", "18:18", "18:30",
-    "18:55", "19:45", "21:30", "22:37", "23:01" "23:59", "02:33", "04:23", "06:46"];
+    "18:55", "19:41", "21:20", "22:37", "23:59", "02:33", "04:23", "06:46"];
 
   scheduleOpportunities(bot, horarios);
 }
