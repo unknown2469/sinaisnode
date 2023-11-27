6822187fdb5a24f009e54463f29f2e900c92ca6f
unknown2469 <152039395+unknown2469@users.noreply.github.com>
2023-11-26 21:30:43 -0300

    Update index.jsaaa

--- index.js
+++ index.js
@@ -79,6 +79,9 @@ function main() {
   // Configuração do webhook
   const app = express();
   const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+   console.log(`Servidor está ouvindo na porta ${PORT}`);
+});
 
   app.use(bodyParser.json());
 
@@ -95,7 +98,7 @@ function main() {
 
   const horarios = ["07:04", "09:50", "10:55", "11:37", "11:45", "12:49", "13:47", "14:02",
     "16:10", "16:17", "16:41", "17:31", "17:45", "18:18", "18:30",
-    "18:55", "19:41", "21:20", "22:37", "23:59", "02:33", "04:23", "06:46"];
+    "18:55", "19:41", "21:30", "22:37", "23:59", "02:33", "04:23", "06:46"];
 
   scheduleOpportunities(bot, horarios);
 }
